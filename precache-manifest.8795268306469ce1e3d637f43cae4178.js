1f5674f4163df1c57cfe387bfc5627de40c7c408
nick momotenko <nickmomotenkoforweb@gmail.com>
2020-05-06 13:32:27 +0300

    Updates

---
+++ precache-manifest.8795268306469ce1e3d637f43cae4178.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3397006603360da7c87dda534a028a1b",
+    "url": "/react-mess-chat/index.html"
+  },
+  {
+    "revision": "b375b8f9e759e9ec0c79",
+    "url": "/react-mess-chat/static/css/main.2133a6b6.chunk.css"
+  },
+  {
+    "revision": "336816e01614b1dcfab5",
+    "url": "/react-mess-chat/static/js/2.384a183f.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/react-mess-chat/static/js/2.384a183f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b375b8f9e759e9ec0c79",
+    "url": "/react-mess-chat/static/js/main.0458114f.chunk.js"
+  },
+  {
+    "revision": "14b8a1a4a5d8e64ddead",
+    "url": "/react-mess-chat/static/js/runtime-main.25002d34.js"
+  },
+  {
+    "revision": "0e0b08bd2538d01ce174bde88c660860",
+    "url": "/react-mess-chat/static/media/camera.0e0b08bd.svg"
+  },
+  {
+    "revision": "2927f8de4268bae8f11c4dfe3adc72b5",
+    "url": "/react-mess-chat/static/media/edit.2927f8de.svg"
+  },
+  {
+    "revision": "a41bc29dcd72280d62c863fd552906ef",
+    "url": "/react-mess-chat/static/media/empty-data.a41bc29d.png"
+  },
+  {
+    "revision": "4c9ab10e1ccc1ef5477c3d5d53510f1d",
+    "url": "/react-mess-chat/static/media/film.4c9ab10e.svg"
+  },
+  {
+    "revision": "23280db63090bf2dfe57d85a8539ab60",
+    "url": "/react-mess-chat/static/media/header-s.23280db6.svg"
+  },
+  {
+    "revision": "6f6464e6de50a52de16c9335691e455e",
+    "url": "/react-mess-chat/static/media/link.6f6464e6.svg"
+  },
+  {
+    "revision": "887feffc881bdf9b4366eb31ac214916",
+    "url": "/react-mess-chat/static/media/list-2.887feffc.svg"
+  },
+  {
+    "revision": "ddfd7f5fb8f46a3d1f64da158e4f2a42",
+    "url": "/react-mess-chat/static/media/micro.ddfd7f5f.svg"
+  },
+  {
+    "revision": "e06790d92db014cbafe39a5cfcbd95b3",
+    "url": "/react-mess-chat/static/media/microphone.e06790d9.svg"
+  },
+  {
+    "revision": "77ae7f2905d39413798ecbd9f56fae96",
+    "url": "/react-mess-chat/static/media/mute.77ae7f29.svg"
+  },
+  {
+    "revision": "3bcd7b006ffa9555dc853f3f68dc78e4",
+    "url": "/react-mess-chat/static/media/option-b.3bcd7b00.svg"
+  },
+  {
+    "revision": "5a80abc1c9b48ff3816614c79442b9c2",
+    "url": "/react-mess-chat/static/media/pause.5a80abc1.svg"
+  },
+  {
+    "revision": "887e38424f0d6e90676026b7070c2ba9",
+    "url": "/react-mess-chat/static/media/photo.887e3842.svg"
+  },
+  {
+    "revision": "7ce4d8a9bb8780b540fff0abd748de19",
+    "url": "/react-mess-chat/static/media/play.7ce4d8a9.svg"
+  },
+  {
+    "revision": "1cb3ec246b67e702a86ee4dc056ff51b",
+    "url": "/react-mess-chat/static/media/send.1cb3ec24.svg"
+  },
+  {
+    "revision": "816317e4a6c373f40bf1881493a49d12",
+    "url": "/react-mess-chat/static/media/smile.816317e4.svg"
+  },
+  {
+    "revision": "2b8196a51fd641948d8c31ba25b52db0",
+    "url": "/react-mess-chat/static/media/wave.2b8196a5.svg"
+  }
+]);
